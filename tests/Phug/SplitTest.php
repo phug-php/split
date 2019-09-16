527b94993b1d653f7aa6146cb7e8476daab04148
KyleKatarn <kylekatarnls@gmail.com>
2019-09-16 18:03:55 +0200

    [Travis-CI] Test shell output in Travis

---
+++ tests/Phug/SplitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Phug\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class SplitTest extends TestCase
+{
+    public function testShellOutput1()
+    {
+        $script = sys_get_temp_dir().'/script.sh';
+        file_put_contents($script, "#!/bin/sh\necho $'something'");
+        chmod($script, 0777);
+        $output = shell_exec(escapeshellcmd($script));
+        unlink($script);
+
+        $this->assertSame('something', $output);
+    }
+
+    public function testShellOutput2()
+    {
+        $script = sys_get_temp_dir().'/script.sh';
+        file_put_contents($script, "#!/bin/sh\necho $\"something\"");
+        chmod($script, 0777);
+        $output = shell_exec(escapeshellcmd($script));
+        unlink($script);
+
+        $this->assertSame('something', $output);
+    }
+
+    public function testShellOutput3()
+    {
+        $script = sys_get_temp_dir().'/script.sh';
+        file_put_contents($script, "#!/bin/sh\necho 'something\n\\Yoh\n\n\'la\' hop\\'");
+        chmod($script, 0777);
+        $output = shell_exec(escapeshellcmd($script));
+        unlink($script);
+
+        $this->assertSame("something\n\\Yoh\n\n'la' hop\\", $output);
+    }
+}
