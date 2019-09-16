57c6d3c6d47dc9c07ab508697cd768b7ca958012
KyleKatarn <kylekatarnls@gmail.com>
2019-09-16 18:12:51 +0200

    [Travis-CI] Debug tests

--- tests/Phug/SplitTest.php
+++ tests/Phug/SplitTest.php
@@ -9,29 +9,18 @@ class SplitTest extends TestCase
     public function testShellOutput1()
     {
         $script = sys_get_temp_dir().'/script.sh';
-        file_put_contents($script, "#!/bin/sh\necho $'something'");
+        file_put_contents($script, "#!/bin/sh\necho $'something\n\\\\Yoh\n\n\\'la\\' hop\\\\'");
         chmod($script, 0777);
         $output = shell_exec(escapeshellcmd($script));
         unlink($script);
 
-        $this->assertSame('something', $output);
+        $this->assertSame("something\n\\Yoh\n\n'la' hop\\", $output);
     }
 
     public function testShellOutput2()
     {
         $script = sys_get_temp_dir().'/script.sh';
-        file_put_contents($script, "#!/bin/sh\necho $\"something\"");
-        chmod($script, 0777);
-        $output = shell_exec(escapeshellcmd($script));
-        unlink($script);
-
-        $this->assertSame('something', $output);
-    }
-
-    public function testShellOutput3()
-    {
-        $script = sys_get_temp_dir().'/script.sh';
-        file_put_contents($script, "#!/bin/sh\necho 'something\n\\Yoh\n\n\'la\' hop\\'");
+        file_put_contents($script, "#!/bin/sh\necho 'something\n\\\\Yoh\n\n\\'la\\' hop\\\\'");
         chmod($script, 0777);
         $output = shell_exec(escapeshellcmd($script));
         unlink($script);
